8c1c052ed98dd0f6b138121fafe4da97861d384b
MarkLindstorff <MLAGit@protonmail.com>
2026-01-02 16:35:57 +0100

    character counter function added. changed where files are opened to main.py instead of stats.py

--- stats.py
+++ stats.py
@@ -1,6 +1,11 @@
-def get_word_count(file_path):
-    with open(file_path) as file:
-        text = file.read()
-
+def get_word_count(text):
     words = text.split()
     return len(words)
+
+def character_counter(text):
+    char_dict = {}
+    lower_text = text.lower()
+
+    for character in lower_text:
+        char_dict[character] = char_dict.get(character, 0) + 1
+    return char_dict
