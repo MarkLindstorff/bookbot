ea9fbfdf4de17ac588ee15bd49cbf58debb7a608
MarkLindstorff <MLAGit@protonmail.com>
2026-01-02 18:33:44 +0100

    Report in-progress

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def main():
     text = get_book_text(book_path)
     total_words = get_word_count(text)
     chars = character_counter(text)
-    
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
     print(chars)
 
 
